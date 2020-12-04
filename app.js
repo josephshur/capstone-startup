9b427aee741f5d1372633c9f9b0ec827b7f44c94
Joseph Shur <joehshur@gmail.com>
2020-12-03 16:47:00 -0800

    Users can edit/update movies

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 
-// Initialize express
-const express = require('express');
+const express = require('express')
+const methodOverride = require('method-override')
 const app = express();
 
 const bodyParser = require('body-parser');
-// require handlebars
 const exphbs = require('express-handlebars');
 
 const models = require('./db/models');
 
-// Use "main" as our default layout
+app.use(methodOverride('_method'))
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
-// Use handlebars to render
+
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,14 +25,7 @@ var movies = [
   { title: "Star Wars: Revenge of the Sith", desc: "As the Clone Wars draw to an end, the Sith secretly rise to power.", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// Mock array 2
-// var comments = [
-//   { title: "Abc", desc: "abc"},
-//   { title: "Abc", desc: "abc"},
-//   { title: "Abc", desc: "abc"}
-// ]
-
-// Index
+// INDEX
 app.get('/', (req, res) => {
   models.Movie.findAll({ order: [['createdAt', 'DESC']] }).then(movies => {
     res.render('movies-index', { movies: movies });
@@ -53,10 +46,39 @@ app.post('/movies', (req, res) => {
   });
 })
 
-// Choose a port to listen on
+// SHOW
+app.get('/movies/:id', (req, res) => {
+  models.Movie.findByPk(req.params.id).then((movie) => {
+    res.render('movies-show', { movie: movie })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// EDIT
+app.get('/movies/:id/edit', (req, res) => {
+  models.Movie.findByPk(req.params.id).then((movie) => {
+    res.render('movies-edit', { movie: movie });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// UPDATE
+app.put('/movies/:id', (req, res) => {
+  models.Movie.findByPk(req.params.id).then(movie => {
+    movie.update(req.body).then(movie => {
+      res.redirect(`/movies/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
 const port = process.env.PORT || 3000;
 
-// Tell the app what port to listen on
 app.listen(port, () => {
   console.log('App listening on port 3000!')
 })
