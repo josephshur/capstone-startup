b24e59823fed8a83608177511ad93fbb3f4f7a69
Joseph Shur <joehshur@gmail.com>
2020-12-03 16:52:30 -0800

    Separation of concerns

--- app.js
+++ app.js
@@ -25,57 +25,7 @@ var movies = [
   { title: "Star Wars: Revenge of the Sith", desc: "As the Clone Wars draw to an end, the Sith secretly rise to power.", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// INDEX
-app.get('/', (req, res) => {
-  models.Movie.findAll({ order: [['createdAt', 'DESC']] }).then(movies => {
-    res.render('movies-index', { movies: movies });
-  })
-})
-
-// NEW
-app.get('/movies/new', (req, res) => {
-  res.render('movies-new');
-})
-
-// CREATE
-app.post('/movies', (req, res) => {
-  models.Movie.create(req.body).then(movie => {
-    res.redirect(`/`);
-  }).catch((err) => {
-    console.log(err)
-  });
-})
-
-// SHOW
-app.get('/movies/:id', (req, res) => {
-  models.Movie.findByPk(req.params.id).then((movie) => {
-    res.render('movies-show', { movie: movie })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-
-// EDIT
-app.get('/movies/:id/edit', (req, res) => {
-  models.Movie.findByPk(req.params.id).then((movie) => {
-    res.render('movies-edit', { movie: movie });
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
-// UPDATE
-app.put('/movies/:id', (req, res) => {
-  models.Movie.findByPk(req.params.id).then(movie => {
-    movie.update(req.body).then(movie => {
-      res.redirect(`/movies/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
+require('./controllers/events')(app, models);
 
 const port = process.env.PORT || 3000;
 
---
+++ controllers/movies.js
@@ -0,0 +1,66 @@
+//movies.js
+
+module.exports = function (app, models) {
+
+  // INDEX
+  app.get('/', (req, res) => {
+    models.Movie.findAll({ order: [['createdAt', 'DESC']] }).then(movies => {
+      res.render('movies-index', { movies: movies });
+    })
+  })
+
+  // NEW
+  app.get('/movies/new', (req, res) => {
+    res.render('movies-new');
+  })
+
+  // CREATE
+  app.post('/movies', (req, res) => {
+    models.Movie.create(req.body).then(movie => {
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err)
+    });
+  })
+
+  // SHOW
+  app.get('/movies/:id', (req, res) => {
+    models.Movie.findByPk(req.params.id).then((movie) => {
+      res.render('movies-show', { movie: movie })
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
+
+  // EDIT
+  app.get('/movies/:id/edit', (req, res) => {
+    models.Movie.findByPk(req.params.id).then((movie) => {
+      res.render('movies-edit', { movie: movie });
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  });
+
+  // UPDATE
+  app.put('/movies/:id', (req, res) => {
+    models.Movie.findByPk(req.params.id).then(movie => {
+      movie.update(req.body).then(movie => {
+        res.redirect(`/movies/${req.params.id}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+  // DELETE
+  app.delete('/movies/:id', (req, res) => {
+    models.Movie.findByPk(req.params.id).then(movie => {
+      movie.destroy();
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  })
+}
